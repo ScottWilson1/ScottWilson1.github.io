991e0564778f1392c440d4fcab162bd935b65854
Scott Wilson <scottwilson84@gmail.com>
2020-12-30 19:46:07 -0500

    committing files that weren't added last time

--- methods.js
+++ methods.js
@@ -1,11 +1,16 @@
 function openNav() {
-    if(screen.width < 760) {
+    // if(screen.width < 760) {
         document.getElementById("side-nav").style.width = "240px";
-    }
+    // }
 }
 
 function closeNav() {
-    if(screen.width < 760) {
+    // if(screen.width < 760) {
         document.getElementById("side-nav").style.width = "0px";
-    }
+    // }
+}
+
+function getScreenWidth() {
+    var sceenWidth = screen.width;
+    document.getElementById("currentWidth").innerHTML = screenWidth;
 }
