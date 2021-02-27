b76542a847ed526e78448960dd2ee0e5cd1a8908
Scott Wilson <scottwilson84@gmail.com>
2021-02-27 00:30:41 -0500

    updated screenshot images

--- methods.js
+++ methods.js
@@ -1,11 +1,11 @@
 function openNav() {
-    if(window.innerWidth < 800) {
+    if(window.innerWidth < 840) {
         document.getElementById("side-nav").style.width = "200px";
     }
 }
 
 function closeNav() {
-    if(window.innerWidth < 800) {
+    if(window.innerWidth < 840) {
         document.getElementById("side-nav").style.width = "0px";
     }
 }
